badf6c25102aa2a59388a2027b278cdf9fd015a1
Aarya2892 <aaryapatil2892@gamil.com>
2025-12-11 07:43:13 +0530

    frontend

---
+++ frontend/my-app/src/App.js
@@ -0,0 +1,32 @@
+import logo from './logo.svg';
+import './App.css';
+import axios from "axios"
+import { useEffect, useState } from 'react';
+
+function App() {
+  const [Jokes, setjokes] = useState([])
+
+  useEffect(()=>{
+    axios.get("http://localhost:3000/jokes")
+    .then((response)=>{
+      setjokes(response.data)
+    })
+    .catch((error)=>[
+      console.log("error")
+    ])
+  })
+  return (
+    <>
+    <h1>Full stack demo</h1>
+    <p>Jokes: {Jokes.length}</p>
+    {Jokes.map((joke, index)=>{
+      <div key={joke.id}>
+        <h2>{joke.title}</h2>
+        <p>{joke.content}</p>
+      </div>
+    })}
+    </>
+  );
+}
+
+export default App;
